fc748ef1798d582a4adbe9f145bcdc70b2107cee
dragonexpert <arceus@teamdimensional.net>
2021-01-11 09:56:53 -0600

    Create tools_nopermission.lang.php

---
+++ inc/languages/english/admin/tools_nopermission.lang.php
@@ -0,0 +1,10 @@
+<?php
+$l['nopermission_info_name'] = "No Permission Log";
+$l['nopermission_info_description'] = "Creates a log when a user encounters a No Permission Page. The log can be found under the Tools tab.";
+$l['nopermission_username'] = "Username";
+$l['nopermission_guest'] = "Guest";
+$l['nopermission_time'] = "Time";
+$l['nopermission_file'] = "File";
+$l['nopermission_url'] = "Url";
+$l['nopermission_admin_permission'] = "Can view No Permission Log";
+$l['no_permission_info_donate'] = "Donate";
