96864a8521ef3ce15598739c7121568653b971e3
dragonexpert <arceus@teamdimensional.net>
2021-02-23 15:18:34 -0600

    Issue #2 Issue #4
    Issue #2 Issue #4

--- inc/plugins/nopermission/db.php
+++ inc/plugins/nopermission/db.php
@@ -13,7 +13,18 @@ function nopermission_db_install()
     uid INT NOT NULL DEFAULT 0,
     dateline BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
     file TEXT,
-    location TEXT) ENGINE=InnoDB" . $db->build_create_table_collation());
+    location TEXT,
+    ipaddress VARBINARY NOT NULL DEFAULT ''
+    ) ENGINE=InnoDB" . $db->build_create_table_collation());
+}
+
+function nopermission_db_update()
+{
+    global $db;
+    if(!$db->field_exists("ipaddress", "nopermission"))
+    {
+        $db->add_column("nopermission", "ipaddress", "VARBINARY NOT NULL DEFAULT ''");
+    }
 }
 
 function nopermission_db_uninstall()
